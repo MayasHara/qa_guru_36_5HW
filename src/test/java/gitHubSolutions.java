d3816c5f7c9a42628fb924f99aeb4e4fa43fe0c0
Владислав <tataurov.vlad@gmail.com>
2025-06-24 13:51:33 +0900

    Optimize class HeaderMenu-link

--- src/test/java/gitHubSolutions.java
+++ src/test/java/gitHubSolutions.java
@@ -12,7 +12,7 @@ public class gitHubSolutions {
         нужная страница (например, что заголовок: "The AI-powered developer platform.").
          */
         open("https://github.com");
-        $$(".HeaderMenu-link.border-0.width-full.width-lg-auto.px-0.px-lg-2.py-lg-2.no-wrap.d-flex.flex-items-center.flex-justify-between.js-details-target").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-link.border-0.width-full.width-lg-auto.px-0.px-lg-2.py-lg-2").findBy(text("Solutions")).hover();
         $$(".HeaderMenu-dropdown-link.d-block.no-underline.position-relative.py-2.Link--secondary").findBy(text("Enterprises")).click();
         $(".Primer_Brand__Hero-module__Hero___EM3jf").shouldHave(text("The AI-powered\n" + "developer platform"));
     }
