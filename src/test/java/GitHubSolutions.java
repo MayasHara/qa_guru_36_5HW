bbcfbdb8d4677893f5e4af55a1fe14407136a785
MayasHara <tataurov.vlad@gmail.com>
2025-06-29 13:38:56 +0900

    rename class + simple css

--- src/test/java/GitHubSolutions.java
+++ src/test/java/GitHubSolutions.java
@@ -1,11 +1,14 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
-public class gitHubSolutions {
+public class GitHubSolutions {
+
     @Test
     void gitHub() {
         /*
@@ -14,26 +17,28 @@ public class gitHubSolutions {
         нужная страница (например, что заголовок: "The AI-powered developer platform.").
          */
         open("https://github.com");
-        $$(".HeaderMenu-link.border-0.width-full.width-lg-auto.px-0.px-lg-2.py-lg-2").findBy(text("Solutions")).hover();
-        $$(".HeaderMenu-dropdown-link.d-block.no-underline.position-relative.py-2.Link--secondary").findBy(text("Enterprises")).click();
+        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprises")).click();
         $(".Primer_Brand__Hero-module__Hero___EM3jf").shouldHave(text("The AI-powered\n" + "developer platform"));
     }
+
     @Test
-     void drag_and_drop () {
+    void drag_and_drop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDrop(to("#column-b"));
     }
+
     @Test
-    void moveToElement () throws InterruptedException {
+    void moveToElement() throws InterruptedException {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         SelenideElement dragBox = $("#column-a");
         SelenideElement dropBox = $("#column-b");
 
         actions()
                 .clickAndHold(dragBox)
-                    .moveToElement(dropBox)
-                        .release()
-                            .build()
-                                .perform();
+                .moveToElement(dropBox)
+                .release()
+                .build()
+                .perform();
     }
 }
